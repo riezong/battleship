10b784d801c01096d485fa67a4fd72df4a958629
Andrew <71823699+riezong@users.noreply.github.com>
2025-09-22 14:34:24 +1000

    Restructure playRound()

--- src/game.js
+++ src/game.js
@@ -8,6 +8,7 @@ const Game = function () {
 	const cpu = new Player('CPU');
 
 	let currentPlayer = human;
+	let targetPlayer = cpu;
 
 	const init = (function () {
 		human.gameboard.placeShip(2, 5, 9);
@@ -39,7 +40,9 @@ const Game = function () {
 	const checkGameOver = function (player1, player2) {
 		if (player1.gameboard.allShipsSunk() || player2.gameboard.allShipsSunk()) {
 			console.log(`${currentPlayer.name} wins!`);
-			return;
+			return true;
+		} else {
+			return false;
 		}
 	};
 
@@ -55,21 +58,29 @@ const Game = function () {
 		let randomMoveY = Math.floor(Math.random() * human.gameboard.gridSize);
 		human.gameboard.receiveAttack(randomMoveX, randomMoveY);
 		domManager.renderBoard(human, 'human-board');
+
+		switchPlayer();
 	};
 
 	const playRound = function (x, y) {
 		console.log(currentPlayer);
 		if (currentPlayer === human) {
-			cpu.gameboard.receiveAttack(x, y);
-			domManager.renderBoard(cpu, 'cpu-board');
-			checkGameOver(human, cpu);
-			switchPlayer();
+			humanPlayerMove(x, y);
+			console.log(checkGameOver(human, cpu));
+			if (checkGameOver(human, cpu) === false) {
+				setTimeout(() => {
+					cpuPlayerMove();
+				}, 500);
+				checkGameOver(human, cpu);
+			}
 		}
-		setTimeout(() => {
-			cpuPlayerMove();
-		}, 500);
-		checkGameOver(human, cpu);
-		switchPlayer();
+		// else if (currentPlayer === cpu) {
+		// 	setTimeout(() => {
+		// 		cpuPlayerMove();
+		// 	}, 500);
+		// }
+
+		// checkGameOver(human, cpu);
 	};
 
 	// playRound();
